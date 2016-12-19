f05d0b65bee58a0b9b068d5e196f103af5557ab7
NEYS <never_eat_yellow_snow1@AC_forums>
2016-12-19 15:35:02 +0100

    fix line endings of .sh script :-(
    Signed-off-by: NEYS <never_eat_yellow_snow1@<AC_forums>>

--- create_release.sh
+++ create_release.sh
@@ -1,20 +1,17 @@
-#!/bin/bash
 
 echo Setting environment
 export PYTHONPATH=$PWD:$PWD/stracker/externals
 
-	(
-	  echo Cleaning up old version
-	  cd stracker
-	  rm -f stracker_linux_x86.tgz
-	  rm -rf dist
-	  rm -rf build
-	  pyinstaller --clean -y -s --exclude-module http_templates --hidden-import cherrypy.wsgiserver.wsgiserver3 --hidden-import psycopg2 --additional-hooks-dir=$PWD/pyinstaller-hooks/ stracker.py
+echo Cleaning up old version
+cd stracker
+rm -f stracker_linux_x86.tgz
+rm -rf dist
+rm -rf build
+pyinstaller --clean -y -s --exclude-module http_templates --hidden-import cherrypy.wsgiserver.wsgiserver3 --hidden-import psycopg2 --additional-hooks-dir=$PWD/pyinstaller-hooks/ stracker.py
 
-	  mv dist/stracker dist/stracker_linux_x86
-	  tar cvzf stracker_linux_x86.tgz -C dist stracker_linux_x86
-	  rm -rf dist
-	  rm -rf build
-      )
+mv dist/stracker dist/stracker_linux_x86
+tar cvzf stracker_linux_x86.tgz -C dist stracker_linux_x86
+rm -rf dist
+rm -rf build
 
 exit 0
